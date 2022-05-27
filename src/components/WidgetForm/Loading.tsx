7b12846a6b29225e7af2cfd37d5acded974da386
Leandro Elias Goerck <leandro@DESKTOP-FJ3FMC8.localdomain>
2022-05-27 18:01:09 -0300

    finished front End

--- src/components/WidgetForm/Loading.tsx
+++ src/components/WidgetForm/Loading.tsx
@@ -2,7 +2,7 @@ import { CircleNotch } from "phosphor-react";
 
 export function Loading() {
   return (
-    <div>
+    <div className="w-6 h-6 flex items-center justify-center animate-spin">
       <CircleNotch weight="bold" className="w-4 h-4" />
     </div>
   );
--- src/components/WidgetForm/index.tsx
+++ src/components/WidgetForm/index.tsx
@@ -6,6 +6,7 @@ import ideaImageUrl from '../../assets/idea.svg'
 import thoughtImageUrl from '../../assets/thought.svg'
 import { FeedbackTypeStep } from "./Steps/FeedbackTypeStep";
 import { FeedbackContentStep } from './Steps/FeedbackContentStep';
+import { FeedbackSuccesStep } from './Steps/FeedbackSuccesStep';
 
 export const feedbackTypes = {
   BUG: {
@@ -35,26 +36,36 @@ export type FeedbackType = keyof typeof feedbackTypes;
 
 export function WidgetForm() {
   const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null);
+  const [feedbackSent, setFeedbackSent] = useState(false);
 
   function handleRestartFeedback() {
+    setFeedbackSent(false);
     setFeedbackType(null);
   }
 
   return (
     <div className="bg-zinc-900 p-4 relative rounded-2xl flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
 
+    { feedbackSent ? (
+      <FeedbackSuccesStep onFeedbackRestartRequested={handleRestartFeedback}/>
+    ) : (
+      <>
+        {!feedbackType ? 
+        
+        <FeedbackTypeStep       
+          onFeedbackTypeChanged={setFeedbackType}
+        
+        />
+      
+      : <FeedbackContentStep
+          feedbackType={feedbackType}
+          onFeedbackRestartRequested={handleRestartFeedback}
+          onFeedbackSent={() => setFeedbackSent(true)}
+      />}
+      </>
+    )}
 
-    {!feedbackType ? 
-    
-      <FeedbackTypeStep       
-        onFeedbackTypeChanged={setFeedbackType}
-       
-      />
-    
-    : <FeedbackContentStep
-        feedbackType={feedbackType}
-        onFeedbackRestartRequested={handleRestartFeedback}
-    />}
+
 
       
 
